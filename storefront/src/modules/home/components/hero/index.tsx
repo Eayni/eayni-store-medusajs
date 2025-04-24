70196acc264e06d97bcca161e396a3ec39ab6d8d
Ahmed Khaled <ahmedkhaled4d@gmail.com>
2025-04-24 03:00:30 +0200

    image logo

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -1,10 +1,18 @@
-import { Github } from "@medusajs/icons"
-import { Button, Heading } from "@medusajs/ui"
+import { Heading } from "@medusajs/ui"
+import Image from "next/image"
 
 const Hero = () => {
   return (
     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
+        <Image
+          src="/logo.png"
+          alt="hero"
+          width={500}
+          height={200}
+          className="w-full h-full object-cover object-center"
+        />
+        <div className="absolute inset-0 bg-ui-bg-base opacity-50" />
         <span>
           <Heading
             level="h1"
@@ -16,10 +24,7 @@ const Hero = () => {
             level="h2"
             className="text-3xl leading-10 text-ui-fg-subtle font-normal"
           >
-            Eayni recycling association cooperative , we manufacture a new life
-            , We work to restore ecosystems for sustainable development to
-            ensure quality of life and society and leadership at the national
-            level in the field of waste recycling and treatment.
+            Shop Now and be a part of the change!
           </Heading>
         </span>
       </div>
